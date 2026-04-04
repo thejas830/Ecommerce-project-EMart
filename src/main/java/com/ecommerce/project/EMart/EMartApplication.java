4c010fc0196b642d94006f85ebd71dcea7e9976b
Thejas <54901184+thejas830@users.noreply.github.com>
2026-04-04 12:33:10 +0530

    Ecommerce project creation

---
+++ src/main/java/com/ecommerce/project/EMart/EMartApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.EMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/project/EMart/EMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.EMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
